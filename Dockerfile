6add7761da029819d36be24a8032b854b3aaef06
Sean Harkins <sharkinsgis@gmail.com>
2020-08-03 20:14:41 -0500

    Roll back to GDAL 3.0 image for testing.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM osgeo/gdal:ubuntu-full-latest
+FROM osgeo/gdal:ubuntu-full-3.0.3
+
+# Required for click with Python 3.6
+ENV LC_ALL=C.UTF-8
+ENV LANG=C.UTF-8
 
 RUN apt-get update
 RUN apt-get install python3-pip -y
@@ -6,10 +10,7 @@ RUN apt-get install python3-pip -y
 RUN pip3 install rasterio==1.1.3 --no-binary rasterio
 RUN pip3 install tox
 RUN apt-get install build-essential python3-dev python3-numpy libhdf4-dev -y
-ARG test=3
 COPY ./ ./hls_hdf_to_cog
 
 ENTRYPOINT ["/bin/sh", "-c"]
 CMD ["cd hls_hdf_to_cog && tox -r"]
-
-
--- tests/test_hls_hdf_to_cog.py
+++ tests/test_hls_hdf_to_cog.py
@@ -6,13 +6,13 @@ from hls_hdf_to_cog.hls_hdf_to_cog import main
 current_dir = os.path.dirname(__file__)
 test_dir = os.path.join(current_dir, "data")
 
-# granule_basename = "HLS.S29.T12XWF.2020071T191201.v1.5{}"
 granule_basename = "HLS.S30.T01LAH.2020097T222759.v1.5{}"
 
 
 def test_hls_hdf_to_cog():
     inputfile = os.path.join(test_dir, granule_basename.format(".hdf"))
     runner = CliRunner()
-    result = runner.invoke(main, [inputfile, "--output-dir", test_dir])
+    result = runner.invoke(main, [inputfile, "--output-dir", test_dir,
+                                  "-p", "deflate"])
     print(result.exception)
     assert result.exit_code == 0
